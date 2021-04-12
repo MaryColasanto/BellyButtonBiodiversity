54ae3a62992803395dfd66dcdaf546a06a8a1faf
MaryColasanto <colasanto.mary@gmail.com>
2021-04-11 19:17:47 -0500

    Added a bar chart to the dashboard based on the top 10 bacteria strains found in a particular individual.

--- static/js/app.js
+++ static/js/app.js
@@ -7,5 +7,21 @@ d3.json("../../data/samples.json").then((data) => {
         option.text(value);
     });
 
-    console.log(data.samples)
+    data.samples.forEach(function(value) {
+        if (value.id === "940") {
+            var yAxis = value.otu_ids.map(y => "OTU ID:" + y);
+            console.log(value.otu_labels[0])
+            var traceDefault = {
+                x: value.sample_values.slice(0,10).reverse(),
+                y: yAxis.slice(0,10).reverse(),
+                type: "bar",
+                orientation: "h",
+                text: value.otu_labels.slice(0,10).reverse(),
+            };
+
+            plotDefault = [traceDefault];
+            Plotly.newPlot("bar", plotDefault)
+        };
+
+    });
 });
